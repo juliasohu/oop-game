7e17f8e0fab9ec1d56bd58caf1b24c1077ebdebb
Julia Solias <juliasohu@gmail.com>
2025-01-24 10:57:37 +0100

    Updated styling and player class

--- js/main.js
+++ js/main.js
@@ -1,1 +1,49 @@
 console.log("hello world");
+
+// CREATION OF NEW CLASS PLAYER //
+
+class Player {
+    constructor(){
+        this.positionX = 10;
+        this.positionY = 0;
+        this.width = 20;
+        this.height = 10;
+
+        this.playerElm = document.getElementById("player")
+        this.updateUI()
+    }
+
+    // ADDING DINAMIC POSITIONING/STYLING ACCORDING TO CLASS //
+    updateUI(){
+        this.playerElm.style.left = this.positionX + "vw"
+        this.playerElm.style.bottom = this.positionY + "vh"
+        this.playerElm.style.width = this.width + "vw"
+        this.playerElm.style.height = this.height + "vh"
+    }
+
+    moveLeft(){
+        this.positionX--;    
+        this.updateUI()
+    }
+
+    moveRight(){
+        this.positionX++;
+        this.updateUI()
+    }
+}
+
+// ADDING NEW PLAYER(S) //
+
+const player = new Player()
+
+// EVENT LISTENER FOR MOVING RIGHT & LEFT //
+
+document.addEventListener("keydown", (event) => {
+    if (event.code === 'ArrowLeft') {
+        player.moveLeft()
+    }
+    if (event.code === 'ArrowRight') {
+        player.moveRight()
+    }
+    
+});
